cd097b1caf22ec603539c5d6dabe0aabd6191949
Esraa Gaber <esraagaber2397@gmail.com>
2021-01-19 00:30:48 +0200

    Update letters1.js

--- letters1.js
+++ letters1.js
@@ -5,6 +5,7 @@ var toy3 = document.getElementById("toy3");
 var toy4 = document.getElementById("toy4");
 var msg = document.getElementById("msg");
 var nxtbtn = document.getElementById("popup");
+var bkbtn = document.getElementById("back");
 
 var tryagain = new Audio();
 var clap = new Audio();
@@ -26,10 +27,14 @@ clap.src="Clap.mp3"
   function msgout(){
    window.location.href="letters2.html";
   }
+  function bkfun(){
+   location.replace('letters0.html');
+   }
 
   toy1.addEventListener('click',wrong)
   toy2.addEventListener('click',wrong)
   toy3.addEventListener('click',right)
   toy4.addEventListener('click',wrong)
   nxtbtn.addEventListener('click',msgout)
-  
+  bkbtn.addEventListener('click',bkfun)
+
