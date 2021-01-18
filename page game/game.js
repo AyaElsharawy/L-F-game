d86e6a5478c74f055fe6c5c82b467b50c925c57e
Aya Elsharawi <34321395+AyaElsharawy@users.noreply.github.com>
2021-01-18 19:32:48 +0200

    css and javascript for game page

---
+++ page game/game.js
@@ -0,0 +1,8 @@
+const center=document.querySelector('.center');
+const center_fall=document.querySelector('.center_fall');
+center.addEventListener('click', () =>{
+    location.replace('flappy_bird.html')
+})
+center_fall.addEventListener('click', () =>{
+    location.replace('falling_ball.html')
+})
