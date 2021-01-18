9457e7d32794560875d6f2fca5fb1db10c19fa8f
Esraa Gaber <esraagaber2397@gmail.com>
2021-01-18 19:37:31 +0200

    Update letters.js

--- letters.js
+++ letters.js
@@ -4,23 +4,29 @@ var toy2 = document.getElementById("toy2");
 var toy3 = document.getElementById("toy3");
 var toy4 = document.getElementById("toy4");
 var msg = document.getElementById("msg");
+var nxtbtn = document.getElementById("popup");
 
-
-
-function fun(){
-   msg.innerHTML='choose the Apple!!'
-}
+var tryagain = new Audio();
+var clap = new Audio();
+tryagain.src="uhoh.mp3"
+clap.src="Clap.mp3"
 
 function wrong(){
     msg.innerHTML='Try Again, choose the Apple!!'
+   tryagain.play();
 }
 
 function right(){
-    
    msg.innerHTML='Great Job'
+   clap.play();
+   nxtbtn.style.display="block"
+}
+function msgout(){
+   window.location.href="letters1.html";
 }
 
 toy1.addEventListener('click',right)
 toy2.addEventListener('click',wrong)
 toy3.addEventListener('click',wrong)
 toy4.addEventListener('click',wrong)
+nxtbtn.addEventListener('click',msgout)
